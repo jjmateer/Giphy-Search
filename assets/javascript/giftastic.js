504b47384babf14a23ff0629d3ab1ac616aef3bb
John Mateer <jj.mateer@live.com>
2019-07-18 20:59:49 -0700

    Created the displayAnimalInfo function that defines the queryURL and makes an ajax call for the specific button being pressed. It then dynamically creates divs  with <p> and <img> tags to hold the ratings of the gifs and the gif itself. So far, the retrieved data comes up as undefined. At the bottom of the page, added an on click function for all the buttons with the class animalButton

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,19 +1,47 @@
 //Initial array of animals
 var animals = ["dog", "cat", "mouse", "lion", "elephant"];
 
+function displayAnimalInfo() {
+    var animal = $(this).attr("data-name");
+    var queryUrl= "http://api.giphy.com/v1/gifs/search?" + animal + "&api_key=NU4CtgobR6DQrOPgFkEn1MAdKV6cmqN7";
+    //Create ajax call for the specific animalBtn being clicked
+    $.ajax({
+        url:queryUrl,
+        method:"GET"
+    }).then(function(response) {
+        //Div to hold the animal
+        var animalDiv = $("<div class='animalDiv'>")
+        //Store rating data
+        var rating = response.Rated;
+        //Element to display the rating
+        var ratingP = $("<p>").text("Rating: " + rating);
+        animalDiv.append(ratingP);
+        var imgURL = response.gif;
+        //create elemtn tot hol the image
+        var image = $("<img>").attr("src", imgURL);
+        animalDiv.append(image);
+        $("#animalDisplay").prepend(animalDiv);
+        console.log(ratingP)
+        
+    });
+
+
+}
+
+
 function renderButtons() {
     //Make sure there are no duplicate buttons
     $("#buttonDisplay").empty();
     //Loop through animals array
     for (var i = 0; i < animals.length; i++) {
         //Dynamically generate buttons for each animal
-        var animalButton = $("<button>");
-        animalButton.addClass("animal");
+        var a = $("<button>");
+        a.addClass("animalButton");
         //Add data attribute with value of animals at index i
-        animalButton.attr("data-name", animals[i]);
+        a.attr("data-name", animals[i]);
         //Add text inside button
-        animalButton.text(animals[i]);
-        $("#buttonDisplay").append(animalButton)
+        a.text(animals[i]);
+        $("#buttonDisplay").append(a);
     }
 }
 
@@ -22,12 +50,16 @@ $("#submitBtn").on("click", function (event) {
     //Prevents form from trying to submit itself
     event.preventDefault();
     //Get text from input
-    var animal = $("#animalInput").val().trim();
+    var animalBtn = $("#animalInput").val().trim();
     //Add text from input to array
-    animals.push(animal);
+    animals.push(animalBtn);
     console.log(animals);
     renderButtons();
 });
+
+
+$(document).on("click", ".animalButton", displayAnimalInfo);
+//Display the initial buttons
 renderButtons();
 
 
