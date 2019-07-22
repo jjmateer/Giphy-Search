5a862803f9559f8da1de3a246103d63caec5e961
John Mateer <jj.mateer@live.com>
2019-07-22 15:36:10 -0700

    Re created the image section of the displayAnimalInfo section by making seperate variables that hold different sources for animate and still. After that, I added attributes that change the images source using the variables above. In the if else statements, I used a normal function instead of an on clickand used the defined attributes to start or stop depending on the current status of the gif. At the bottom of the page, I added an on click function for the animalImage that calls on the startStopGif function.

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -3,81 +3,94 @@ var animals = ["dog", "cat", "mouse", "lion", "elephant"];
 
 function displayAnimalInfo() {
     var animal = $(this).attr("data-name");
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + 
-    animal + "&api_key=NU4CtgobR6DQrOPgFkEn1MAdKV6cmqN7";
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" +
+        animal + "&api_key=NU4CtgobR6DQrOPgFkEn1MAdKV6cmqN7";
     //Create ajax call for the specific animalBtn being clicked
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).done(function (response) {
         $("#animalDisplay").empty();
-        for (var i = 0;i < 10; i++) {
+        for (var i = 0; i < 10; i++) {
             var animalDiv = $("<div>").addClass("animalDiv")
             console.log(response.data[i]);
             var results = response.data[i];
+            //Get the rating and append it to the screen with the divs
             var rating = results.rating;
             var ratingP = $("<p>").text("Rating: " + results.rating);
             animalDiv.append(ratingP);
+            //variable that dynamically creates an image element 
+            var image = $("<img>");
+            //variable with the source of an animated gif
+            var imgAnimate = results.images.fixed_height.url;
+            //variable with the source of a static gif
+            var imgStill = results.images.fixed_height_still.url;
             
-            
-            var imgURL = results.images.fixed_height.url;
-            imgURL = results.images.fixed_width.url;
-            //create elemtn tot hol the image
-            var image = $("<img>").attr("src", imgURL).addClass("animalImage");
+            //Add attributes that can be used in the if else statements to start
+            //and stop the gifs.
+            image.addClass("animalImage");
+            image.attr("src", imgStill);
+            image.attr("data-state", "still");
+            image.attr("data-still", imgStill);
+            image.attr("data-animate", imgAnimate);
+            //Append the image to the animal div and prepend animalDiv to #animalDisplay
             animalDiv.append(image);
             $("#animalDisplay").prepend(animalDiv);
+
         }
     });
 }
-function renderButtons() {
-    //Make sure there are no duplicate buttons
-    $("#buttonDisplay").empty();
-    //Loop through animals array
-    for (var i = 0; i < animals.length; i++) {
-        //Dynamically generate buttons for each animal
-        var a = $("<button>");
-        a.addClass("animalButton");
-        //Add data attribute with value of animals at index i
-        a.attr("data-name", animals[i]);
-        //Add text inside button
-        a.text(animals[i]);
-        $("#buttonDisplay").append(a);
+    //If else statements to determine if the gif is animated or still and
+    //change it to the opposite attribute.
+    function startStopGif() {
+        console.log(state);
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+
+        }
     }
-}
+    function renderButtons() {
+        //Make sure there are no duplicate buttons
+        $("#buttonDisplay").empty();
+        //Loop through animals array
+        for (var i = 0; i < animals.length; i++) {
+            //Dynamically generate buttons for each animal
+            var a = $("<button>");
+            a.addClass("animalButton");
+            //Add data attribute with value of animals at index i
+            a.attr("data-name", animals[i]);
+            //Add text inside button
+            a.text(animals[i]);
+            $("#buttonDisplay").append(a);
 
-
-
-
-$(".animalImage").on("click", function () {
-    console.log(state);
-    var state = $(this).attr("data-state");
-    if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-    } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
+        }
     }
 
-});
 
-//On-click function for submitBtn
-$("#submitBtn").on("click", function (event) {
-    //Prevents form from trying to submit itself
-    event.preventDefault();
-    //Get text from input
-    var animalBtn = $("#animalInput").val().trim();
-    //Add text from input to array
-    animals.push(animalBtn);
-    console.log(animals);
+    //On-click function for submitBtn
+    $("#submitBtn").on("click", function (event) {
+        //Prevents form from trying to submit itself
+        event.preventDefault();
+        //Get text from input
+        var animalBtn = $("#animalInput").val().trim();
+        //Add text from input to array
+        animals.push(animalBtn);
+        console.log(animals);
+        renderButtons();
+
+    });
+
+    //Event listener for animalButton to display the gifs and rating to the page.
+    $(document).on("click", ".animalButton", displayAnimalInfo);
+    //Event listener for animalImage to start or stop the gif on click.
+    $(document).on('click', ".animalImage", startStopGif);
+    //Display the initial buttons
     renderButtons();
-
-});
-
-
-$(document).on("click", ".animalButton", displayAnimalInfo);
-//Display the initial buttons
-renderButtons();
 
 
 
